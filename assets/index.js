1562be60a76243aa33007d903d008c29484c2575
OtienohOwinoh <coowinoh@gmail.com>
2022-08-31 12:28:16 +0300

    fetch parameters

--- assets/index.js
+++ assets/index.js
@@ -4,6 +4,7 @@ const container = document.querySelector ('container');
 let searchQuery = '';
 const APP_ID ='3acb26b3';
 const APP_key = '3b074862a1b0a5a50c91b34c1c53ba25';
+const baseURL = `https://api.edamam.com/api/recipes/v2?q=sausage&app_id=${APP_ID}&APP_key=${APP_key}`;
 
 searchForm.addEventListener ('submit', (e) =>{
     e.preventDefault();
