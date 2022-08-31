5b566fc8141c99a0516ec88dffafafeb08bc3840
OtienohOwinoh <coowinoh@gmail.com>
2022-08-31 12:04:27 +0300

    SEarch querry to variables

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,12 @@
 const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector ('.search-result');
-const container = document.querySelector ('container')
+const container = document.querySelector ('container');
+let searchQuery = '';
+
+searchForm.addEventListener ('submit', (e) =>{
+    e.preventDefault();
+    searchQuery = e.target.querySelector('input').value;
+
+    console.log (searchQuery)
+}
+)
