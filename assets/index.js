38df1df585cc03c2bb638bdb46c3465d6d958609
OtienohOwinoh <coowinoh@gmail.com>
2022-08-31 15:57:00 +0300

    Dom COntent Event LIstener

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,31 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
 const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector ('.search-result');
 const container = document.querySelector ('container');
 let searchQuery = '';
-const APP_ID ='3acb26b3';
+const APP_ID = '3acb26b3';
 const APP_key = '3b074862a1b0a5a50c91b34c1c53ba25';
-const baseURL = `https://api.edamam.com/api/recipes/v2?q=sausage&app_id=${APP_ID}&APP_key=${APP_key}`;
+
+
 
 searchForm.addEventListener ('submit', (e) =>{
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
 
+    fetchAPI();
+});
+
+async function fetchAPI () {
+    const baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_key}`;
+    const response = await fetch(baseURL)
+    
+    console.log (response);
 }
-)
+
+
+// https://api.edamam.com/search?q=pizza&app_id=3acb26b3$app_key=${APP_key}
+
+
+})
