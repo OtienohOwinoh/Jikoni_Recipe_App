4db91447b73394d07f95df6c084caa7554f9f9bd
OtienohOwinoh <coowinoh@gmail.com>
2022-08-31 12:19:15 +0300

    API Keys

--- assets/index.js
+++ assets/index.js
@@ -2,11 +2,12 @@ const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector ('.search-result');
 const container = document.querySelector ('container');
 let searchQuery = '';
+const APP_ID ='3acb26b3';
+const APP_key = '3b074862a1b0a5a50c91b34c1c53ba25';
 
 searchForm.addEventListener ('submit', (e) =>{
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
 
-    console.log (searchQuery)
 }
 )
